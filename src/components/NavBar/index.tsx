fe146ceaadcf5fdb7358bdf4ce2711d5d8fe077a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-30 00:52:17 +0100

    adding navbar & header

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,25 @@
+import { ChevronDown, Logo } from "../Icons"
+
 const NavBar = () => {
-  return <nav>qc</nav>
+  return (
+    <nav>
+      <div className="logo__container">
+        <Logo />
+      </div>
+      <div className="nav__list">
+        <ul>
+          <li>Prices</li>
+          <li>Learn</li>
+          <li className="resources">
+            Resources <ChevronDown />
+          </li>
+          <li>Blog</li>
+          <li>Sign In</li>
+        </ul>
+        <button className="primary__button">Free Trial</button>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
