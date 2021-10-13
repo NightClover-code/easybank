6a9b76ec3401920c8b5f9da2779a96a45cbbb5af
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-13 19:30:50 +0100

    adding footer social icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,19 @@
+import { LogoIcon } from '../Icons';
+
 const Footer = () => {
-  return <footer>qlk</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer__head">
+          <div className="logo__container">
+            <LogoIcon />
+          </div>
+          <p>The next generation of digital banking</p>
+          <div className="socials"></div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
