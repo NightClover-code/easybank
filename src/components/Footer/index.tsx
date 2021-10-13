cb9923d7af7b7a92a3288ff6cdf9ba9e9cd428b9
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-13 20:55:13 +0100

    styling footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,54 +4,56 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div className="footer__head">
-          <div className="logo__container">
-            <LogoIcon />
-          </div>
-          <p>The next generation of digital banking</p>
-          <div className="socials">
-            <div className="social__box">
-              <FacebookIcon />
+        <div className="flex">
+          <div className="footer__head">
+            <div className="logo__container">
+              <LogoIcon />
             </div>
-            <div className="social__box">
-              <TwitterIcon />
-            </div>
-            <div className="social__box">
-              <LinkedInIcon />
+            <p>The next generation of digital banking</p>
+            <div className="socials">
+              <div className="social__box">
+                <FacebookIcon />
+              </div>
+              <div className="social__box">
+                <TwitterIcon />
+              </div>
+              <div className="social__box">
+                <LinkedInIcon />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="footer__lists">
-          <div className="first__group">
-            <ul>
-              <li className="title">Company</li>
-              <li>About</li>
-              <li>Blog</li>
-              <li>Terms of Use</li>
-              <li>Privacy Policy</li>
-              <li>How it works</li>
-            </ul>
-            <ul>
-              <li className="title">Features</li>
-              <li>Screen Sharing</li>
-              <li>iOS & Android Apps</li>
-              <li>File Sharing</li>
-              <li>User Management</li>
-            </ul>
-          </div>
-          <div className="second__group">
-            <ul>
-              <li className="title">Support</li>
-              <li>FAQ</li>
-              <li>Live Chat</li>
-              <li>Customer Service</li>
-            </ul>
-            <ul>
-              <li className="title">Contact</li>
-              <li>info@easybank.com</li>
-              <li>1-898-615-123</li>
-              <li>3500 Sunset BLC, Creek Rd, CA </li>
-            </ul>
+          <div className="footer__lists">
+            <div className="first__group list__group">
+              <ul>
+                <li className="title">Company</li>
+                <li>About</li>
+                <li>Blog</li>
+                <li>Terms of Use</li>
+                <li>Privacy Policy</li>
+                <li>How it works</li>
+              </ul>
+              <ul>
+                <li className="title">Features</li>
+                <li>Screen Sharing</li>
+                <li>iOS & Android Apps</li>
+                <li>File Sharing</li>
+                <li>User Management</li>
+              </ul>
+            </div>
+            <div className="second__group list__group">
+              <ul>
+                <li className="title">Support</li>
+                <li>FAQ</li>
+                <li>Live Chat</li>
+                <li>Customer Service</li>
+              </ul>
+              <ul>
+                <li className="title">Contact</li>
+                <li>info@easybank.com</li>
+                <li>1-898-615-123</li>
+                <li>3500 Sunset BLC, Creek Rd, CA </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="separator"></div>
