f0c302aad92aa38df7c0256257aa18b4f7566f7b
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-27 23:51:27 +0100

    cleanup

---
+++ src/utils/contentfulHandlers.ts
@@ -0,0 +1,70 @@
+import {
+  AdvantageInterface,
+  FeatureInterface,
+  HeroInterface,
+  TestimonialInterface,
+} from '../interfaces';
+import { sortByDate } from '.';
+
+export const dataToFeatures = (data: any): FeatureInterface[] => {
+  return sortByDate(
+    data.allContentfulFeature.edges.map(
+      ({ node: { title, description, image, createdAt, aosDelay } }: any) => ({
+        title,
+        aosDelay,
+        iconURL: image.icon.file.url,
+        description: description.description,
+        image: {
+          alt: image.alt,
+          url: image.icon.file.url,
+        },
+        createdAt,
+      })
+    )
+  );
+};
+
+export const dataToAdvantages = (data: any): AdvantageInterface[] => {
+  return sortByDate(
+    data.allContentfulAdvantage.edges.map(
+      ({ node: { title, description, image, createdAt, topic } }: any) => ({
+        title,
+        description: description.description,
+        image: {
+          alt: image.alt,
+          url: image.icon.file.url,
+        },
+        createdAt,
+        topic,
+      })
+    )
+  );
+};
+
+export const dataToTestimonials = (data: any): TestimonialInterface[] => {
+  return sortByDate(
+    data.allContentfulTestimonial.edges.map(
+      ({ node: { name, content, image, createdAt } }: any) => ({
+        name,
+        content: content.content,
+        image: {
+          alt: image.alt,
+          url: image.icon.file.url,
+        },
+        createdAt,
+      })
+    )
+  );
+};
+
+export const dataToHero = (data: any): HeroInterface => {
+  return data.allContentfulHero.edges.map(
+    ({ node: { description, image } }: any) => ({
+      description: description.description,
+      image: {
+        alt: image.alt,
+        url: image.icon.file.url,
+      },
+    })
+  )[0];
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,73 +1,3 @@
-import {
-  AdvantageInterface,
-  FeatureInterface,
-  HeroInterface,
-  TestimonialInterface,
-} from '../interfaces';
-
-export const dataToFeatures = (data: any): FeatureInterface[] => {
-  return sortByDate(
-    data.allContentfulFeature.edges.map(
-      ({ node: { title, description, image, createdAt, aosDelay } }: any) => ({
-        title,
-        aosDelay,
-        iconURL: image.icon.file.url,
-        description: description.description,
-        image: {
-          alt: image.alt,
-          url: image.icon.file.url,
-        },
-        createdAt,
-      })
-    )
-  );
-};
-
-export const dataToAdvantages = (data: any): AdvantageInterface[] => {
-  return sortByDate(
-    data.allContentfulAdvantage.edges.map(
-      ({ node: { title, description, image, createdAt, topic } }: any) => ({
-        title,
-        description: description.description,
-        image: {
-          alt: image.alt,
-          url: image.icon.file.url,
-        },
-        createdAt,
-        topic,
-      })
-    )
-  );
-};
-
-export const dataToTestimonials = (data: any): TestimonialInterface[] => {
-  return sortByDate(
-    data.allContentfulTestimonial.edges.map(
-      ({ node: { name, content, image, createdAt } }: any) => ({
-        name,
-        content: content.content,
-        image: {
-          alt: image.alt,
-          url: image.icon.file.url,
-        },
-        createdAt,
-      })
-    )
-  );
-};
-
-export const dataToHero = (data: any): HeroInterface => {
-  return data.allContentfulHero.edges.map(
-    ({ node: { description, image } }: any) => ({
-      description: description.description,
-      image: {
-        alt: image.alt,
-        url: image.icon.file.url,
-      },
-    })
-  )[0];
-};
-
 export const sortByDate = (arr: any[]) => {
   return arr
     .sort(
@@ -81,3 +11,4 @@ export const isEven = (n: number) => n % 2 === 0;
 
 export * from './styles';
 export * from './configs';
+export * from './contentfulHandlers';
